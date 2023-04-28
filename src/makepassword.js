fba3d9b545d67458394149229d4283a305f0a343
Angle-M <98354046+Angle-M@users.noreply.github.com>
2023-04-28 17:20:11 -0400

    ran into issues with pervious code

--- src/makepassword.js
+++ src/makepassword.js
@@ -1,23 +1,30 @@
 const fs = require('fs');
 const crypto = require('crypto');
-const password_file = 'password.txt';
-const encrypted_file = 'password.enc.txt';
+
 // Choose a hash algorithm
 const hash_algo = 'sha256';
-function makepassword(password_file,encrypted_file) {
-    password_file = fs.readFileSync('password.txt');
-    encrypted_file = fs.createWriteStream('password.enc.txt');
-    const lines = password_file.trim().split('\n');
+
+function makepassword(password_file, encrypted_file) {
+    // Read the password file
+    const password_data = fs.readFileSync(password_file, 'utf8');
+
+    // Split the file into lines
+    const lines = password_data.trim().split('\n');
+
+    // Hash the passwords and write to the encrypted file
+    const encrypted_stream = fs.createWriteStream(encrypted_file);
     for (let line of lines) {
         const [email, password] = line.split(':');
         const hash = crypto.createHash(hash_algo).update(password).digest('hex');
-        encrypted_file.write(`${email}:${hash}\n`);
+        encrypted_stream.write(`${email}:${hash}\n`);
     }
-    encrypted_file.end();
+    encrypted_stream.end();
 
     console.log('Encryption completed successfully.');
 }
-if (require.main === module){
-    makepassword('/src/password.txt', '/src/password.enc.txt');
+
+if (require.main === module) {
+    makepassword('./password.txt', './password.enc.txt');
 }
-module.exports = {makepassword};
+
+module.exports = { makepassword };
